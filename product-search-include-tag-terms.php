71ecfdc9b0738e121832b598543a79af1a5e12bf
Sean Conklin <seanconklin@yahoo.com>
2018-10-09 11:54:04 -0700

    Fix to product search by tag code

--- product-search-include-tag-terms.php
+++ product-search-include-tag-terms.php
@@ -4,21 +4,21 @@
 add_filter( 'the_posts', 'storefront_child__search_by_product_tag', 10, 2 );
 
 function storefront_child__search_by_product_tag( $posts, $query = false ) {
-  	global $post_type;
-    $is_product = isset( $post_type ) && is_array( $post_type )
-        ? false === in_array( 'product', $post_type ) : false;
+	global $post_type;
+	//$is_product = isset( $post_type ) && is_array( $post_type )
+	//	? false === in_array( 'product', $post_type ) : false;
 
 	// Only for searches of products
-	if( ! is_search() || ! is_main_query() || ! $query || ! $is_product ) {
+	if( ! is_search() || ! is_main_query() || ! $query ) {
 		return $posts;
 	}
 
 	// Product tags search
 	$args = [
-	  'post_type' => 'product',
-	  'posts_per_page' => 5,
-	  'product_tag' => get_search_query(),
-	  'post__not_in' => wp_list_pluck( $posts, 'ID' ),
+		'post_type' => 'product',
+		'posts_per_page' => 5,
+		'product_tag' => get_search_query(),
+		'post__not_in' => wp_list_pluck( $posts, 'ID' ),
 	];
 	$results = get_posts( $args );
 
