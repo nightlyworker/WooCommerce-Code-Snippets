e32d67bd516c8bbcdf9ebeb88bed8d24dd3fff6c
Sean Conklin <seanconklin@yahoo.com>
2018-10-13 12:33:35 -0700

    Added categories search to product tags search snippet

--- product-search-include-tag-terms.php
+++ product-search-include-tag-terms.php
@@ -4,19 +4,16 @@
 add_filter( 'the_posts', 'storefront_child__search_by_product_tag', 10, 2 );
 
 function storefront_child__search_by_product_tag( $posts, $query = false ) {
-	global $post_type;
-	//$is_product = isset( $post_type ) && is_array( $post_type )
-	//	? false === in_array( 'product', $post_type ) : false;
 
-	// Only for searches of products
-	if( ! is_search() || ! is_main_query() || ! $query ) {
+	// Require Current Search
+	if( ! is_search() ) {
 		return $posts;
 	}
 
 	// Product tags search
 	$args = [
 		'post_type' => 'product',
-		'posts_per_page' => 5,
+		'posts_per_page' => 200,
 		'product_tag' => get_search_query(),
 		'post__not_in' => wp_list_pluck( $posts, 'ID' ),
 	];
@@ -25,7 +22,21 @@ function storefront_child__search_by_product_tag( $posts, $query = false ) {
 	// Store any new results
 	if( $results ) {
 		$posts = array_merge( $posts, $results );
-		$query->posts = $posts;
+		$query->found_posts = sizeof( $posts );
+	}
+
+   	// Product categories search
+	$args = [
+		'post_type' => 'product',
+		'posts_per_page' => 200,
+		'product_cat' => get_search_query(),
+		'post__not_in' => wp_list_pluck( $posts, 'ID' ),
+	];
+	$results = get_posts( $args );
+
+	// Store any new results
+	if( $results ) {
+		$posts = array_merge( $posts, $results );
 		$query->found_posts = sizeof( $posts );
 	}
 
